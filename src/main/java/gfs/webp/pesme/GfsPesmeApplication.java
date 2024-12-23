78c2efabc7c819b7a3c7d71c62239f9830cea2e0
Novke <novke.brate@gmail.com>
2024-12-23 21:33:41 +0100

    mapper pom + bean

--- src/main/java/gfs/webp/pesme/GfsPesmeApplication.java
+++ src/main/java/gfs/webp/pesme/GfsPesmeApplication.java
@@ -1,13 +1,20 @@
 package gfs.webp.pesme;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GfsPesmeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GfsPesmeApplication.class, args);
+    }
+
+    @Bean
+    ModelMapper mapper(){
+        return new ModelMapper();
     }
 
 }
