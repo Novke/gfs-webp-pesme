ad048bb598dc2ff2e7e68428b4e2c68420b5213d
Novke <novke.brate@gmail.com>
2024-12-23 13:47:07 +0100

    init

---
+++ src/main/java/gfs/webp/pesme/GfsPesmeApplication.java
@@ -0,0 +1,13 @@
+package gfs.webp.pesme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GfsPesmeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GfsPesmeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gfs/webp/pesme/GfsPesmeApplicationTests.java
@@ -0,0 +1,13 @@
+package gfs.webp.pesme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GfsPesmeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
