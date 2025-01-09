a3dc2884822dd4bb1618d30851e218327be1bc9b
Novke <novke.brate@gmail.com>
2025-01-09 16:42:59 +0100

    poruka dobrodoslice

--- src/main/java/gfs/webp/pesme/GfsPesmeApplication.java
+++ src/main/java/gfs/webp/pesme/GfsPesmeApplication.java
@@ -4,7 +4,11 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
+
+import static gfs.webp.pesme.util.PrintUtils.*;
 
 @SpringBootApplication
 public class GfsPesmeApplication {
@@ -21,6 +25,20 @@ public class GfsPesmeApplication {
                 .setSkipNullEnabled(true)
                 .setMatchingStrategy(MatchingStrategies.STRICT);
         return mapper;
+    }
+
+
+    @EventListener(ApplicationReadyEvent.class)
+    void start(){
+        ispisiCeoRedJednaLinija();
+        ispisiBanner();
+        ispisiCeoRedJednaLinija();
+        ispisiCeoRedDveLinije();
+        ispisiUokvireno("COPYRIGHT - Gradjevinski Fakultet u Subotici, Univerzitet U Novom Sadu");
+        ispisiCeoRedDveLinije();
+        ispisiUokvireno("API URL: http://localhost:8080");
+        ispisiUokvireno("Dokumentacija se nalazi na: http://localhost:8080/swagger-ui/index.html");
+        ispisiCeoRedDveLinije();
     }
 
 }
---
+++ src/main/java/gfs/webp/pesme/util/PrintUtils.java
@@ -0,0 +1,68 @@
+package gfs.webp.pesme.util;
+
+public class PrintUtils {
+
+    private static Integer SIRINA = 150;
+
+    public static void ispisiCeoRedKarakterom(String text){
+        for (int i = 0; i < SIRINA; i++) System.out.print(text);
+        System.out.println();
+    }
+
+    public static void ispisiCeoRedJednaLinija(){
+        ispisiCeoRedKarakterom("-");
+    }
+    public static void ispisiCeoRedDveLinije(){
+        ispisiCeoRedKarakterom("=");
+    }
+
+    public static void ispisiUokvireno(String text, int debljinaRama){
+        if (debljinaRama<1) debljinaRama = 1;
+        if (debljinaRama>10) debljinaRama = 10;
+
+        for (int i = 0; i < debljinaRama; i++) System.out.print("=");
+
+        System.out.print("  ");
+        System.out.print(text);
+
+        for (int i = debljinaRama * 2 + 4 + text.length(); i < SIRINA; i++){
+            System.out.print(" ");
+        }
+
+        System.out.print("  ");
+        for (int i = 0; i < debljinaRama; i++) System.out.print("=");
+        System.out.println();
+    }
+
+    public static void ispisiUokvireno(String text){
+        ispisiUokvireno(text, 3);
+    }
+
+    public static void ispisiBanner(){
+        String text = """
+              
+                                                                                                     \s
+                                                                                                     \s
+                        GGGGGGGGGGGGG        FFFFFFFFFFFFFFFFFFFFFF           SSSSSSSSSSSSSSS        \s
+                     GGG::::::::::::G        F::::::::::::::::::::F         SS:::::::::::::::S       \s
+                   GG:::::::::::::::G        F::::::::::::::::::::F        S:::::SSSSSS::::::S       \s
+                  G:::::GGGGGGGG::::G        FF::::::FFFFFFFFF::::F        S:::::S     SSSSSSS       \s
+                 G:::::G       GGGGGG          F:::::F       FFFFFF        S:::::S                   \s
+                G:::::G                        F:::::F                     S:::::S                   \s
+                G:::::G                        F::::::FFFFFFFFFF            S::::SSSS                \s
+                G:::::G    GGGGGGGGGG          F:::::::::::::::F             SS::::::SSSSS           \s
+                G:::::G    G::::::::G          F:::::::::::::::F               SSS::::::::SS         \s
+                G:::::G    GGGGG::::G          F::::::FFFFFFFFFF                  SSSSSS::::S        \s
+                G:::::G        G::::G          F:::::F                                 S:::::S       \s
+                 G:::::G       G::::G          F:::::F                                 S:::::S       \s
+                  G:::::GGGGGGGG::::G        FF:::::::FF                   SSSSSSS     S:::::S       \s
+                   GG:::::::::::::::G ...... F::::::::FF            ...... S::::::SSSSSS:::::S ......\s
+                     GGG::::::GGG:::G .::::. F::::::::FF            .::::. S:::::::::::::::SS  .::::.\s
+                        GGGGGG   GGGG ...... FFFFFFFFFFF            ......  SSSSSSSSSSSSSSS    ......\s
+                                                                                                     \s
+                 G R A D J E V I N S K I      F A K U L T E T       U      S U B O T I C I           \s
+                                
+                """;
+        System.out.println(text);
+    }
+}
